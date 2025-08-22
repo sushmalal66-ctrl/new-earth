e126b60916cdc50c471cdb4136b03a9f5828dd22
sushmalal66-ctrl <sushmalal66@gmail.com>
2025-08-22 16:02:46 +0530

    Updated timelines.ts

--- src/animations/timelines.ts
+++ src/animations/timelines.ts
@@ -80,6 +80,9 @@ export const initializeTimelines = (container: HTMLElement) => {
               opacity: 1,
               y: 0,
               duration: 1,
+              ease: "power2.out"
+            }
+          );
         }
       });
     });
