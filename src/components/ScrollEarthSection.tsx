140d77bdb7b9af0b8d82e3448d16e9b20583bcdf
commoder678-prog <commoder678@gmail.com>
2025-08-22 12:03:59 +0530

    Updated ScrollEarthSection.tsx

--- src/components/ScrollEarthSection.tsx
+++ src/components/ScrollEarthSection.tsx
@@ -61,6 +61,7 @@ const ScrollEarthSection: React.FC = () => {
           canvasRef.current.style.willChange = 'transform, opacity';
           canvasRef.current.style.opacity = opacity.toString();
         }
+  }, [isInCloudTransition, cloudOpacity, contentOpacity]);
 
   // Lenis scroll handler - single source of truth
   const lenis = useLenis(({ scroll, limit }) => {
